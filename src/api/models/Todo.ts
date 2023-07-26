0449b87c1499d249dda027b5c7085f87f8ea9448
wagner <wagnerweb2010@gmail.com>
2023-07-26 00:59:56 -0300

    changed description to be optional.

--- src/api/models/Todo.ts
+++ src/api/models/Todo.ts
@@ -1,7 +1,7 @@
 type Todo = {
     id          : number;
     name        : string;
-    description : string;
+    description?: string;
     pub_date    : Date;
     to_date    ?: Date | null;
     done        : boolean;
